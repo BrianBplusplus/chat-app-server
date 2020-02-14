8aa5f322317b45a55925edd10f0574f89f608dd0
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-14 11:01:15 +0100

    added /message path

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ function onListen() {
   console.log(`Listening_on : ${port}`);
 }
 
+const jsonMiddleware = express.json();
+app.use(jsonMiddleware);
+
+app.post("/message", function(request, response) {
+  const { body } = request;
+
+  console.log("request body text", body);
+});
+
 app.listen(port, onListen);
