efe178fdae88f89f5fe0df6ebcc12684a7f2de74
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-14 11:21:12 +0100

    stuff

--- message/router.js
+++ message/router.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const Message = require("./model");
 
 const { Router } = express;
 
 const router = Router();
 
-router.post("/message", function(request, response) {
-  const { body } = request;
+router.post("/message", async function(request, response) {
+  try {
+    const { body } = request;
+    const { text } = body;
+    const entity = { text };
 
-  console.log("request body text", body);
+    const response = await Message.create(entity);
+    console.log(response.dataValues);
+
+    console.log("request body text", body);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
